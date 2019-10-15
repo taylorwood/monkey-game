ae760fa7dfd67633b5d257410bd74e9cdf510bec
Taylor Wood <taylorwood@me.com>
2019-10-15 18:40:17 -0500

    Add flying banana

--- src/monkey_game/core.clj
+++ src/monkey_game/core.clj
@@ -4,47 +4,54 @@
             [clojure.java.io :as io]))
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color  0
-   :images {:monkey (q/load-image "resources/monkey.png")
-            :man    (q/load-image "resources/man.png")
-            :bush   (q/load-image "resources/bush.png")}})
+  {:images
+   {:monkey (q/load-image "resources/monkey.png")
+    :man    (q/load-image "resources/man.png")
+    :bush   (q/load-image "resources/bush.png")
+    :banana (q/load-image "resources/banana.png")}})
+
+(defn update-bananas [bs]
+  (cond
+    (nil? bs)
+    [{:x -100 :y (/ (q/width) 4) :angle 0}]
+    (seq bs)
+    (->> bs
+         (remove #(<= (q/width) (- (:x %) 100)))
+         (map (fn [b]
+                (-> b
+                    (update :angle #(mod (+ 0.2 %) 360))
+                    (update :x #(min (q/width)
+                                     (+ % (rand-int 6))))
+                    (update :y #(min (q/height)
+                                     (+ % (rand-int 3)))))))
+         (seq))))
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  (assoc state
-    :color (mod (+ (:color state) 0.7) 255)))
+  (update state :bananas update-bananas))
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  ; Move origin point to the center of the sketch.
+  (q/fill 0 255 255)
+  (q/image-mode :center)
   (q/with-translation [(/ (q/width) 2)
                        (/ (q/height) 2)]
-    (q/image-mode :center)
-    (q/image (get-in state [:images :man]) 0 0 (/ 837 2.5) (/ 1200 2.5))
+    (q/image (get-in state [:images :man]) 0 0 335 480)
     (q/image (get-in state [:images :bush]) 0 0)
-    (q/image (get-in state [:images :monkey]) 0 (/ (q/height) 4))))
+    (q/image (get-in state [:images :monkey]) 0 (/ (q/height) 4)))
+  (doseq [{:keys [x y angle]} (:bananas state)]
+    (q/push-matrix)
+    (q/translate x y)
+    (q/rotate angle)
+    (q/image (get-in state [:images :banana]) 0 0 100 100)
+    (q/pop-matrix)))
 
 (comment
   (q/defsketch monkey-game
     :title "Hop Monkey"
     :size [500 500]
-    ; setup function called only once, during sketch initialization.
     :setup setup
-    ; update-state is called on each iteration before draw-state.
     :update update-state
     :draw draw-state
     :features [:keep-on-top]
-    ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
     :middleware [m/fun-mode]))
